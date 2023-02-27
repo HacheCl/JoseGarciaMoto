9b7021f62d85dc969d0a85d9b2cda6ac7cbe64c4
Jose Garcia <Jose-Garcia-Paton@gmail.com>
2023-02-27 18:27:59 +0100

    commitMarca

--- src/GIT/JoseGarciaMoto.java
+++ src/GIT/JoseGarciaMoto.java
@@ -1,5 +1,50 @@
 package GIT;
 
 public class JoseGarciaMoto {
+	public String marca;
+	public int numMatr;
+	public int numKm;
+	public String moto;
 
+public JoseGarciaMoto(String marca, String comb, int numMatr,
+		int numKm, String moto) {
+		super();
+		this.marca = marca;
+		this.numMatr = numMatr;
+		this.numKm = numKm;
+		this.moto = moto;
+
+	}
+
+public String getMarca() {
+	return marca;
+}
+
+public void setMarca(String marca) {
+	this.marca = marca;
+}
+
+public int getNumMatr() {
+	return numMatr;
+}
+
+public void setNumMatr(int numMatr) {
+	this.numMatr = numMatr;
+}
+
+public int getNumKm() {
+	return numKm;
+}
+
+public void setNumKm(int numKm) {
+	this.numKm = numKm;
+}
+
+public String getMoto() {
+	return moto;
+}
+
+public void setMoto(String moto) {
+	this.moto = moto;
+}
 }
